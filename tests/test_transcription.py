49d2b77c4f5519e22cce043dd5874b9db965d014
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-10-07 12:44:58 -0400

    transcription works for mono flac and mono wav, as the gcp docs https://cloud.google.com/speech-to-text/docs/encoding say

--- tests/test_transcription.py
+++ tests/test_transcription.py
@@ -9,11 +9,18 @@ from moshiaud import storage, transcribe
 # NOTE must setup logging for TRANSCRIPT to be logged and not error
 setup_loguru()
 
+# NOTE speech to text works for flac and wav only: https://cloud.google.com/speech-to-text/docs/encoding
+
 @pytest.mark.skip("The store client points to demo-test, hardcoded, so this fails with a billing error.")
 def test_transcribe_gs_path(usr_audio: Path, store: Client):
     """Test transcription of a local audio file."""
     # NOTE: This test is not run in CI because it requires a GCP project with
     #       a valid service account key. It is run locally by developers.
+    if usr_audio.suffix not in ('.wav', '.flac'):
+        pytest.skip(f"Skipping test for audio with suffix: {usr_audio.suffix}")
+    if 'mono' not in usr_audio.stem:
+        pytest.skip(f"Google Speech-to-Text only supports mono audio, not stereo. Skipping test for audio with stem: {usr_audio.stem}")
+    print(f"usr_audio={usr_audio}")
     gs_path = Path("tests/data") / usr_audio.name
     storage.upload(usr_audio, gs_path, store)
     transcription = transcribe.transcribe(gs_path, "en-US")
@@ -22,8 +29,11 @@ def test_transcribe_gs_path(usr_audio: Path, store: Client):
 
 def test_transcribe_gs_bytes(usr_audio: Path, store: Client):
     """Test transcription of a local audio file using the transcription's direct bytes functionality."""
-    # NOTE: This test is not run in CI because it requires a GCP project with
-    #       a valid service account key. It is run locally by developers.
+    if usr_audio.suffix not in ('.wav', '.flac'):
+        pytest.skip(f"Skipping test for audio with suffix: {usr_audio.suffix}")
+    if 'mono' not in usr_audio.stem:
+        pytest.skip(f"Google Speech-to-Text only supports mono audio, not stereo. Skipping test for audio with stem: {usr_audio.stem}")
+    print(f"usr_audio={usr_audio}")
     with open(usr_audio, 'rb') as f:
         audio_bytes = f.read()
     transcription = transcribe.transcribe(audio_bytes, "en-US")
